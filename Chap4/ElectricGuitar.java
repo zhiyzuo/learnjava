eb7bbc61b32ae5b6b83649aae3b4f8673415675f
Zhiya Zuo <zhiyazuo@gmail.com>
2016-09-25 23:29:59 -0500

    finish Chap4

---
+++ Chap4/ElectricGuitar.java
@@ -0,0 +1,29 @@
+class ElectricGuitar {
+    String brand;
+    int numOfPickups;
+    boolean rockStarUseIt;
+
+    String getBrand() {
+        return brand;
+    }
+
+    void setBrand(String aBrand) {
+        brand = aBrand;
+    }
+
+    int getNumOfPickups(int num) {
+        return numOfPickups
+    }
+
+    void setNumOfPickups(int num) {
+        numOfPickups = num;
+    }
+
+    boolean getRockStarUseIt() {
+        return rockStarUseIt;
+    }
+
+    void setRockStarUseIt(boolean useIt) {
+        rockStarUseIt = useIt;
+    }
+}
---
+++ Chap4/GoodDogTestDrive.java
@@ -0,0 +1,12 @@
+class GoodDogTestDrive {
+    public static void main (String[] args) {
+        GoodDog one = new GoodDog();
+        one.setSize(70);
+        GoodDog two = new GoodDog();
+        two.setSize(8);
+        System.out.println("Dog one: " + one.getSize());
+        System.out.println("Dog two: " + two.getSize());
+        one.bark();
+        two.bark();
+    }
+}
